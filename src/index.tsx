1c28c8277d761bbadc7100079f6ee93f3742bd82
muzzamil75 <muzamil.ijaz11@gmail.com>
2019-04-17 15:02:13 +0500

    fixed:coverage test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import { createPlugin } from '@bluebase/core';
 import {WebView} from './components/ReactNativeWebView'
 
-export default createPlugin({
 
-	key: 'native-web-view',
+export default createPlugin({
+	description: 'A BlueBase plugin boilerplate!',
+	key: 'nativeWebView',
 	name: 'nativeWebView',
+	version: '1.0.0',
+
 
 	components: {
 		WebView:WebView,
 	},
-
-	
 });
+
